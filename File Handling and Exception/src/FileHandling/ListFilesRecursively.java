57049a13479e265b476416ac45186b9fd67cd2cf
akshay.gaikwad <akshay.gaikwad@AKGA-BLR-01.accolite.com>
2019-06-18 01:05:59 +0530

    File Handling and Exception Assignment

---
+++ File Handling and Exception/src/FileHandling/ListFilesRecursively.java
@@ -0,0 +1,37 @@
+package FileHandling;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ListFilesRecursively {
+	//Below method list all files recursively in directory
+	public static void listAllFiles(File dir)
+	{
+		File[] subdir=dir.listFiles();
+		for(File f:subdir)
+		{
+			if(f.isDirectory())
+			{
+				listAllFiles(f);
+			}
+			else
+			{
+				System.out.println(f.getAbsolutePath());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner inp=new Scanner(System.in);
+		System.out.println("Enter absolute path of directory");
+		String path=inp.nextLine();
+		System.out.println("All files in directory and sub directory are as follows: ");
+		listAllFiles(new File(path));
+		inp.close();
+		
+
+	}
+
+}
+
+
